43e722cdbc455b5e70c1243a8dce4e78257e75f8
Pedro <ppeu.jobs@outlook.com>
2025-06-01 17:08:31 -0300

    adição de nav, main e section

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
-function styles(){
-    return gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(gulp.dest('./build/styles'))
+// Função pra compilar o SCSS
+function styles() {
+  return gulp.src('./src/styles/**/*.scss') // pega todos scss dentro da pasta
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(gulp.dest('./build/styles'));
 }
 
+// Task default (executa só uma vez)
 exports.default = styles;
-exports.watch = function(){
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
-}
+
+// Task de watch (fica observando mudanças)
+exports.watch = function() {
+    gulp.watch('./src/styles/**/*.scss', gulp.parallel(styles));
+};
